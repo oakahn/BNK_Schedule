95998fd6caf9ae1d2c0ccc1d43d5a347ef9605bb
oakahn <mjpoak@gmail.com>
2019-06-26 16:18:54 +0700

    update home

---
+++ BNK_APP/Screen/Home/VC/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  BNK_APP
+//
+//  Created by Chayawat Suntornrak on 25/6/2562 BE.
+//  Copyright © 2562 Chayawat Suntornrak. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
